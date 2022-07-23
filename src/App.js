0d915743b25742412b7b36bb2602191ef517d722
Vasanthi G <vasanthra.g@gmail.com>
2022-07-23 21:55:39 +0530

    changes for automation to succeed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing react with automation
         </a>
       </header>
     </div>
