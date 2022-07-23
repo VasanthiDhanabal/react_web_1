f3feb7543630a245dd5984738bba074d97834fb0
Vasanthi G <vasanthra.g@gmail.com>
2022-07-23 22:15:05 +0530

    Testing on local network mobile

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing react with automation
+          Testing react with automation on mobile
         </a>
       </header>
     </div>
